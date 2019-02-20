9ad25f195c9ea833fcd22c00781aa8b8768295eb
trevjnels <trevorjohnnels@gmail.com>
2019-02-20 10:59:21 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+var STORE = [
+  {
+    question:
+      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
+    questionNumber: 1,
+    answers: [
+      { text: "Candid Thovex", correct: true },
+      { text: "Travice Rice", correct: false },
+      { text: "Angel Collinson", correct: false },
+      { text: "Tom Jones", correct: false }
+    ]
+  },
+  {
+    question:
+      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
+    questionNumber: 2,
+    answers: [
+      { text: "Candid Thovex", correct: true },
+      { text: "Travice Rice", correct: false },
+      { text: "Angel Collinson", correct: false },
+      { text: "Tom Jones", correct: false }
+    ]
+  },
+  {
+    question:
+      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
+    questionNumber: 3,
+    answers: [
+      { text: "Candid Thovex", correct: true },
+      { text: "Travice Rice", correct: false },
+      { text: "Angel Collinson", correct: false },
+      { text: "Tom Jones", correct: false }
+    ]
+  },
+  {
+    question:
+      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
+    questionNumber: 4,
+    answers: [
+      { text: "Candid Thovex", correct: true },
+      { text: "Travice Rice", correct: false },
+      { text: "Angel Collinson", correct: false },
+      { text: "Tom Jones", correct: false }
+    ]
+  },
+  {
+    question:
+      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
+    questionNumber: 5,
+    answers: [
+      { text: "Candid Thovex", correct: true },
+      { text: "Travice Rice", correct: false },
+      { text: "Angel Collinson", correct: false },
+      { text: "Tom Jones", correct: false }
+    ]
+  },
+  {
+    question:
+      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
+    questionNumber: 6,
+    answers: [
+      { text: "Candid Thovex", correct: true },
+      { text: "Travice Rice", correct: false },
+      { text: "Angel Collinson", correct: false },
+      { text: "Tom Jones", correct: false }
+    ]
+  },
+  {
+    question:
+      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
+    questionNumber: 7,
+    answers: [
+      { text: "Candid Thovex", correct: true },
+      { text: "Travice Rice", correct: false },
+      { text: "Angel Collinson", correct: false },
+      { text: "Tom Jones", correct: false }
+    ]
+  }
+];
+
+var currentQuestion = 1;
+var currentScore = 0;
