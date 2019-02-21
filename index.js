5d0433ea550fbb8a2ad9ba89c0c1a8a77be04909
trevjnels <trevorjohnnels@gmail.com>
2019-02-21 10:45:25 -0700

    cannot get the form to not refersh html page upon submit

--- index.js
+++ index.js
@@ -1,82 +1,115 @@
-var STORE = [
-  {
-    question:
-      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
-    questionNumber: 1,
-    answers: [
-      { text: "Candid Thovex", correct: true },
-      { text: "Travice Rice", correct: false },
-      { text: "Angel Collinson", correct: false },
-      { text: "Tom Jones", correct: false }
-    ]
-  },
-  {
-    question:
-      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
-    questionNumber: 2,
-    answers: [
-      { text: "Candid Thovex", correct: true },
-      { text: "Travice Rice", correct: false },
-      { text: "Angel Collinson", correct: false },
-      { text: "Tom Jones", correct: false }
-    ]
-  },
-  {
-    question:
-      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
-    questionNumber: 3,
-    answers: [
-      { text: "Candid Thovex", correct: true },
-      { text: "Travice Rice", correct: false },
-      { text: "Angel Collinson", correct: false },
-      { text: "Tom Jones", correct: false }
-    ]
-  },
-  {
-    question:
-      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
-    questionNumber: 4,
-    answers: [
-      { text: "Candid Thovex", correct: true },
-      { text: "Travice Rice", correct: false },
-      { text: "Angel Collinson", correct: false },
-      { text: "Tom Jones", correct: false }
-    ]
-  },
-  {
-    question:
-      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
-    questionNumber: 5,
-    answers: [
-      { text: "Candid Thovex", correct: true },
-      { text: "Travice Rice", correct: false },
-      { text: "Angel Collinson", correct: false },
-      { text: "Tom Jones", correct: false }
-    ]
-  },
-  {
-    question:
-      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
-    questionNumber: 6,
-    answers: [
-      { text: "Candid Thovex", correct: true },
-      { text: "Travice Rice", correct: false },
-      { text: "Angel Collinson", correct: false },
-      { text: "Tom Jones", correct: false }
-    ]
-  },
-  {
-    question:
-      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
-    questionNumber: 7,
-    answers: [
-      { text: "Candid Thovex", correct: true },
-      { text: "Travice Rice", correct: false },
-      { text: "Angel Collinson", correct: false },
-      { text: "Tom Jones", correct: false }
-    ]
-  }
-];
+function jqueryRunner() {
+  let currentQuestion = 0;
+  let currentScore = 0;
 
-var currentQuestion = 1;
-var currentScore = 0;
+  $(".answers").on("submit", function(e) {
+    e.preventDefault();
+  });
+
+  console.log("jquery is running!");
+  console.log("current question =  ", currentQuestion);
+
+  // $(".answer-select").on("click", function(event) {
+  //
+  //   alert("button clicked");
+  // });
+
+  function screenLoader() {
+    // repalces main element with next question of quiz. if we have not started yet
+    //it will load first
+    if (currentQuestion) {
+      console.log("showing quiz");
+      $(".quiz").show();
+      $(".enter").hide();
+    } else {
+      $(".quiz").hide();
+      $(".enter").show();
+
+      $(".enter > button").on("click", function() {
+        alert("enter button clicked!");
+        currentQuestion++;
+        questionRender();
+      });
+    }
+    console.log("current question =  ", currentQuestion);
+  }
+
+  function questionRender() {
+    // goes into the questionArray (not stor) and renders the appropaite question to the .quiz class element
+    if (currentQuestion) {
+      var question = STORE[currentQuestion - 1];
+      console.log(question);
+      $(".quiz").replaceWith(
+        `<h2 class="question">
+          ${question.question}
+          </h2>
+          <form class="answers">
+            <feildset>
+              <label class="answerChoice">
+                <input type='radio' value=${
+                  question.answers[0].text
+                } name="answer required">
+                <span>${question.answers[0].text}</span>
+              </label>
+              <label class="answerChoice">
+                <input type='radio' value=${
+                  question.answers[1].text
+                } name="answer required">
+                <span>${question.answers[1].text}</span>
+              </label>
+              <label class="answerChoice">
+                <input type='radio' value=${
+                  question.answers[2].text
+                } name="answer required">
+                <span>${question.answers[2].text}</span>
+              </label>
+              <label class="answerChoice">
+                <input type='radio' value=${
+                  question.answers[3].text
+                } name="answer required">
+                <span>${question.answers[3].text}</span>
+              </label>
+              <button class="submitAnswer">Submit</button>
+            </feildset>
+          </form>`
+      );
+      currentQuestion++;
+    } else {
+      console.log("on begin screen");
+    }
+  }
+
+  function questionRandomizer(questions) {
+    //will randomize questions and load them into an array to display
+  }
+
+  function answerRandomizer(question) {
+    // shifts the answers around using randomness so the first one is not always corret
+  }
+
+  function answerListner() {
+    //this will listen for clicks on answers and call correctOrNot
+    $(".submitAnswer").on("submit", function(event) {
+      console.log("working");
+      event.preventDefault();
+    });
+  }
+  function correctOrNot() {}
+
+  function scoreUpdater() {
+    //updates users's score as they go
+  }
+
+  function questionCounter() {
+    //updates user's locaiton in the quiz
+  }
+
+  questionRender();
+  screenLoader();
+  answerListner();
+}
+
+$(jqueryRunner);
+
+// {/* <h2 class="score"><span>Score:</span><span class="currentScore"> 4</span></h2>
+// <h2 class="questionTrack"><span>Question:</span><span class="currentQuestion"> 5/8</span></h2> */}
---
+++ store.js
@@ -0,0 +1,86 @@
+var STORE = [
+  {
+    question:
+      "who is the greatest skier in the world and is famous for making an entire ski video without snow?",
+    questionNumber: 1,
+    answers: [
+      { text: "Candid Thovex", correct: true },
+      { text: "Travice Rice", correct: false },
+      { text: "Angel Collinson", correct: false },
+      { text: "Tom Jones", correct: false }
+    ],
+    img:
+      "https://www.tetongravity.com/images/ci-images/55552/candide_thovex_ski__stream_hero.png"
+  },
+  {
+    question:
+      "What state has two ski resorts that still do not allow snowboarding?",
+    questionNumber: 2,
+    answers: [
+      { text: "Utah", correct: true },
+      { text: "Colorado", correct: false },
+      { text: "Vermont", correct: false },
+      { text: "Wyoming", correct: false }
+    ],
+    img:
+      "http://static1.squarespace.com/static/53714acce4b0bb13e3c90e93/537264d3e4b04835e33db007/5baa7a3224a69467b83ec65a/1539719065906/1839223.jpg?format=1500w"
+  },
+  {
+    question: "Where is Chad's Gap?",
+    questionNumber: 3,
+    answers: [
+      { text: "Little Cottenwood Canyon", correct: true },
+      { text: "Grand Tetons", correct: false },
+      { text: "Baker Pass", correct: false },
+      { text: "Mt. Hood", correct: false }
+    ],
+    img:
+      "http://1.bp.blogspot.com/-xfwBcXYnwdA/T-I7GhJeKvI/AAAAAAAACA4/gyH2kPTuBbI/s1600/618_timdurtschi_chadsgap_brentbenson_ut.jpg"
+  },
+  {
+    question: "What ski company does Candid own?",
+    questionNumber: 4,
+    answers: [
+      { text: "Faction", correct: true },
+      { text: "Armada", correct: false },
+      { text: "Solomon", correct: false },
+      { text: "Burton", correct: false }
+    ],
+    img:
+      "http://www.boardsportsource.com/wp-content/uploads/historic/posts/article_2273/image_1.jpg"
+  },
+  {
+    question: "What major US city has the best access to the Steep and Deep?",
+    questionNumber: 5,
+    answers: [
+      { text: "Salt Lake City, Utah", correct: true },
+      { text: "Denver", correct: false },
+      { text: "Portland", correct: false },
+      { text: "Seattle", correct: false }
+    ],
+    img: "https://admissions.utah.edu/international/_images/saltlakecity.png"
+  }
+  // ,{
+  //   question: "What was the first ski resort in the US to allow snowbording?",
+  //   questionNumber: 6,
+  //   answers: [
+  //     { text: "Brighton, Utah", correct: true },
+  //     { text: "Killington, Vermont", correct: false },
+  //     { text: "Vail, Colorado", correct: false },
+  //     { text: "Taos, New Mexico", correct: false }
+  //   ],
+  //   img: "https://pbs.twimg.com/profile_images/547133020801495040/sJ-1T2JU.png"
+  // },
+  // {
+  //   question: "What Snowboard company makes the HoverCraft?",
+  //   questionNumber: 7,
+  //   answers: [
+  //     { text: "Jones", correct: true },
+  //     { text: "Burton", correct: false },
+  //     { text: "GNU", correct: false },
+  //     { text: "Forum", correct: false }
+  //   ],
+  //   img:
+  //     "http://coresites-cdn.factorymedia.com/whitelines_new/wp-content/uploads/2016/09/jones_hovercraft_snowboard_2016_2017_review_100_T__7739.jpg"
+  // }
+];
