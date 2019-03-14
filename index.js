3dbdc6f22e6667c6d3af2ef65fb1486b9f25a2e8
trevjnels <trevorjohnnels@gmail.com>
2019-03-13 18:26:39 -0600

    this was crap, now its not

--- index.js
+++ index.js
@@ -23,44 +23,56 @@ let quizBuilder = function(database) {
 };
 var quiz = quizBuilder(STORE);
 
+console.log(quiz);
+
 $(".begin-button-js").on("click", function(ev) {
   $(".title > p").hide();
-  renderQuestion();
+  renderQuestion(0);
   renderProgress(currentScore, currentQuestion);
   $(".begin-button-js").hide();
 });
 
 $("body").on("submit", ".the-form", function(ev) {
   ev.preventDefault();
+  $(".quiz-submit").hide();
   let selectedAnswer = $("input[name=answer]:checked").val();
 
   if (correctChecker(selectedAnswer, currentQuestion)) {
     backgroundCorrect(currentQuestion);
+    console.log("you got it right RADICALLLL!!!");
+    $(".question-response").html(`<p>Radical! You got it right.</p>`);
     currentScore++;
+    updateScore(currentScore);
     currentQuestion++;
   } else {
+    $(".question-response").html(
+      `<p>YardSale! The right answer was "${correctAnswerGenerator(
+        currentQuestion
+      )}" breh....</p>`
+    );
     backgroundIncorrect();
     currentQuestion++;
   }
-  if (currentQuestion === 5) {
-    renderProgress(currentScore, currentQuestion);
-    finalScreen(currentScore);
-  } else {
-    renderQuestion();
-    renderProgress(currentScore, currentQuestion);
-  }
+
+  nextHandler(currentScore, currentQuestion);
 });
 
-let correctChecker = function(answer, questionNum) {
+const correctAnswerGenerator = function(questionNum) {
   var correctAnswer = "";
   var questionAnswerSet = quiz[questionNum].answers;
   questionAnswerSet.forEach(function(ans) {
     if (Object.values(ans).indexOf(true) === 1) {
-      correctAnswer = ans.text.split(" ")[0];
+      correctAnswer = ans.text;
     }
   });
+  return correctAnswer;
+};
+
+const correctChecker = function(answer, currentQuestion) {
+  var correctAnswer = correctAnswerGenerator(currentQuestion).split(" ")[0];
   return correctAnswer === answer;
 };
+
 let backgroundCorrect = function(questionNum) {
   img = quiz[questionNum].img;
   $("body").css("background-image", `url(${img})`);
@@ -77,73 +89,119 @@ let backgroundIncorrect = function() {
   $("body").css("background-repeat", "no-repeat");
 };
 let finalScreen = function(currentScore) {
+  $(".questionTrack").hide();
   $(".title > p").show();
+  console.log("current score", currentScore);
+  console.log("currentScoreType", typeof currentScore);
+  $(".main").html(
+    `<div><h2 class="white">Percentage Correct: ${(currentScore / 5) *
+      100}%</h2></div><div class="startover"><a href="index.html"><button class="startoverButton" for="start new quiz">Start New Quiz</button></a></div>`
+  );
 
-  $(".quiz-box").html(
-    `<h1 class="white">Your final results are:<h1><div><h2 class="white">Score: ${(currentScore /
-      5) *
-      100}%</h2></div>`
-  );
-  $("body").append(
-    '<img class="bottom" alt="goofy picture of a skier wearing a halloween costume that makes it look like he is getting a piggy back ride, but the fella giving him the piggyback is just his pants." src="https://i.ebayimg.com/images/i/311974668454-0-1/s-l1000.jpg"></img>'
-  );
-  $(".startover").show();
+  $(".main").toggleClass("mainFinal", true);
+  $(".main").toggleClass("quiz-box", false);
+  // $("body").append(
+  //   '<img class="bottom" alt="goofy picture of a skier wearing a halloween costume that makes it look like he is getting a piggy back ride, but the fella giving him the piggyback is just his pants." src="https://i.ebayimg.com/images/i/311974668454-0-1/s-l1000.jpg"></img>'
+  // );
 };
 
-let renderProgress = function(score, stage) {
-  if (!score) {
-    score = "0";
-  }
-  if (!stage) {
-  }
-  if (stage === 5) {
-    $(".progress")
-      .html(`   <h2 class=" white score"><span>Score:</span><span class="currentScore"> ${score}</span></h2>
-      `);
-  } else {
-    $(".progress")
-      .html(`   <h2 class="white score"><span>Score:</span><span class="currentScore"> ${score}</span></h2>
-      <h2 class=" white questionTrack"><span>Question:</span><span class="currentQuestion">  ${stage +
-        1}/5</span></h2>`);
-  }
+const updateProgress = function(stage) {
+  console.log("stage is", stage);
+  $(".currentQuestion").html(`Question: ${stage + 1}/5`);
 };
-let renderQuestion = function() {
+const updateScore = function(score) {
+  console.log("score is ", score);
+  $(".currentScore").html(`Score: ${score}`);
+};
+const renderProgress = function(score, stage) {
+  $(".progress")
+    .html(`   <div class="progressBar white score currentScore currentScore">Score: 0</div>
+      <div class="progressBar white questionTrack currentQuestion">Question: 1/5</div>`);
+};
+
+const nextHandler = function(currentScore, currentQuestion) {
+  if (currentQuestion === 5) {
+    $(".nextButton").text("Finish");
+  }
+  $(".nextButton").show();
+  $(".the-form").hide();
+
+  $(".nextButton").on("click", function() {
+    if (currentQuestion === 5) {
+      console.log("currentQuestion is ", currentQuestion);
+      updateProgress(currentQuestion);
+      finalScreen(currentScore);
+    } else {
+      renderQuestion(currentQuestion);
+    }
+  });
+
+  // renderQuestion();
+  // finalScreen();
+  // if (currentQuestion === 5) {
+  //   renderProgress(currentScore, currentQuestion);
+  // } else {
+  //   renderProgress(currentScore, currentQuestion);
+  // }
+};
+
+const renderQuestion = function(currentQuestion) {
   let question = quiz[currentQuestion];
+  updateProgress(currentQuestion);
 
-  $(".quiz-box").html(
-    `<div class='container'>
+  $(".nextButton").hide();
+  $(".quiz-submit").show();
+  $(".the-form").show();
+
+  $(".main").html(
+    `
+
         <h2 class="white question">
         ${question.question}
         </h2>
-        <form class='the-form'>
-          <feildset>
-            <label class="answerChoice">
-              <input type='radio' value=${
-                question.answers[0].text
-              } name="answer" required >
-              <span class="answerText">${question.answers[0].text}</span>
-            </label>
-            <label class="answerChoice">
-              <input type='radio' value=${
-                question.answers[1].text
-              } name="answer" required >
-              <span class="answerText">${question.answers[1].text}</span>
-            </label>
-            <label class="answerChoice">
-              <input type='radio' value=${
-                question.answers[2].text
-              } name="answer" required >
-              <span class="answerText">${question.answers[2].text}</span>
-            </label>
-            <label class="answerChoice">
-              <input type='radio' value=${
-                question.answers[3].text
-              } name="answer" required >
-              <span class="answerText">${question.answers[3].text}</span>
-            </label>
-          </feildset>
-          <button class="submit" type="submit">Submit</button>
-        </form>
-      </div>`
+
+            <form class='the-form'>
+              <feildset>
+                <label class="answerChoice">
+                  <input type='radio' value=${
+                    question.answers[0].text
+                  } name="answer" required>
+                  <span class="answerText">${question.answers[0].text}</span>
+                </label>
+                <label class="answerChoice">
+                  <input type='radio' value=${
+                    question.answers[1].text
+                  } name="answer" required>
+                  <span class="answerText">${question.answers[1].text}</span>
+                </label>
+                <label class="answerChoice">
+                  <input type='radio' value=${
+                    question.answers[2].text
+                  } name="answer" required>
+                  <span class="answerText">${question.answers[2].text}</span>
+                </label>
+                <label class="answerChoice">
+                  <input type='radio' value=${
+                    question.answers[3].text
+                  } name="answer" required>
+                  <span class="answerText">${question.answers[3].text}</span>
+                </label>
+              </feildset>
+              <div class="bottombuttons">
+                <button class="submit quiz-submit" type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+            <div class="response">
+              <div class="question-response"></div>
+              <div>
+              <button class="submit nextButton">Next</button></div>
+            
+
+
+
+          `
   );
+  $(".main").toggleClass("quiz-box", true);
+  $(".nextButton").hide();
 };
